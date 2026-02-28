5282aa982e30d96d025f61a3698271041a0ef87a
mukundkumarthakur <mukund_thakur@zohomail.in>
2026-02-28 20:42:30 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,14 @@
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+
+total = first_number + second_number
+subtraction = first_number - second_number
+multiplication = first_number * second_number
+division = first_number / second_number
+
+print("Addition:",total)
+print("Subtraction:",subtraction)
+print("Multiplication:",multiplication)
+print("Division:",division)
+
+
